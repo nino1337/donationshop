1aa7753b936fb5d70317d19ffd02e3ade2a891ae
Nino Dornemann <nino.dornemann@brandung.de>
2018-09-03 15:51:46 +0200

    initial commit - create vue app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="donate-shop">
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/main.scss';
+
+
+</style>
